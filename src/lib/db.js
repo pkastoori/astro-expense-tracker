754d5dd5c5cefb14ebf842bb11ae38816e751396
Pushkar Kastoori <pushkarkastoori@yahoo.com>
2023-08-16 22:28:01 +0530

    Initial commit

---
+++ src/lib/db.js
@@ -0,0 +1,12 @@
+import postgres from "postgres";
+
+const sql = postgres({
+  host: import.meta.env.DATABASE_HOST,
+  port: 5432,
+  database: import.meta.env.DATABASE_NAME,
+  username: import.meta.env.DATABASE_USER,
+  password: import.meta.env.DATABASE_PASSWORD,
+  ssl: "require",
+});
+
+export default sql;
